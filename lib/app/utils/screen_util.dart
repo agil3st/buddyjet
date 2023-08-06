034eb833ddec456fc4122334eb3eb981bce04d08
Agil Setiawan <agilsetiawan772@gmail.com>
2023-08-06 22:02:48 +0700

    Delete screen_util.dart

--- lib/app/utils/screen_util.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ScreenUtil {
-  static Size screenSize(BuildContext context) => MediaQuery.of(context).size;
-  static double width(BuildContext context) => screenSize(context).width;
-  static double height(BuildContext context) => screenSize(context).height;
-  static double aspectRatio(BuildContext context) => screenSize(context).aspectRatio;
-}
