7204f14a242ca8303eaff273938b6f9d390dcbdc
Agil Setiawan <agilsetiawan772@gmail.com>
2023-08-08 22:32:06 +0700

    Update ui_radius.dart

--- lib/app/config/design_system/ui_radius.dart
+++ lib/app/config/design_system/ui_radius.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 
 class UIRadius {
   static Radius get radius2 => const Radius.circular(2);
@@ -12,4 +12,16 @@ class UIRadius {
   static Radius get radius24 => const Radius.circular(24);
   static Radius get radius28 => const Radius.circular(28);
   static Radius get radius32 => const Radius.circular(32);
+
+  static BorderRadiusGeometry get circular2 => BorderRadius.circular(2);
+  static BorderRadiusGeometry get circular4 => BorderRadius.circular(4);
+  static BorderRadiusGeometry get circular6 => BorderRadius.circular(6);
+  static BorderRadiusGeometry get circular8 => BorderRadius.circular(8);
+  static BorderRadiusGeometry get circular10 => BorderRadius.circular(10);
+  static BorderRadiusGeometry get circular14 => BorderRadius.circular(14);
+  static BorderRadiusGeometry get circular18 => BorderRadius.circular(18);
+  static BorderRadiusGeometry get circular20 => BorderRadius.circular(20);
+  static BorderRadiusGeometry get circular24 => BorderRadius.circular(24);
+  static BorderRadiusGeometry get circular28 => BorderRadius.circular(28);
+  static BorderRadiusGeometry get circular32 => BorderRadius.circular(32);
 }
